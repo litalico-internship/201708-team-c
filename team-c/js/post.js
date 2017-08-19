f6da364f3fce4528c01285ce85ba3b0d17beaeb0
tomori <nt24101310@gmail.com>
2017-08-19 21:42:56 +0900

    投稿画面の見た目の完成

--- team-c/js/post.js
+++ team-c/js/post.js
@@ -1,9 +1,1 @@
-/* 変更中（ドラッグ中） */
-$( 'input[type=range]' ).on( 'input', function () {
-	var val = $(this).val();
-} );
 
-/* 変更後 */
-$( 'input[type=range]' ).change( function () {
-	var val = $(this).val();
-} );
