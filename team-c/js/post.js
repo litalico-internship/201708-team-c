8cd1f995e47f8866268d90b97e8bf8f5695fb65f
tomori <nt24101310@gmail.com>
2017-08-19 18:00:59 +0900

    CSSのレイアウトの調整

--- team-c/js/post.js
+++ team-c/js/post.js
@@ -1,1 +1,9 @@
-"use strict";
+/* 変更中（ドラッグ中） */
+$( 'input[type=range]' ).on( 'input', function () {
+	var val = $(this).val();
+} );
+
+/* 変更後 */
+$( 'input[type=range]' ).change( function () {
+	var val = $(this).val();
+} );
