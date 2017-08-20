44daa5b553245750f79362393c6d0affa4c916f0
yugogura <jigenbakdaaan@gmail.com>
2017-08-20 13:51:02 +0900

    投稿画面背景変化

--- team-c/js/post.js
+++ team-c/js/post.js
@@ -32,17 +32,36 @@ $(function () {
 			2: '0.4',
 			3: '0.6',
 			4: '0.8',
-			5: '1.0'
+			5: '0.9'
 		};
-		console.log($("body").css('background-color'));
-		rgba_code = "";
-		if (rgba_code.match(/\,/).index == 3) {
-			//alphaある前提
-		} else {
-			//rgbのみの場合
-
-		}
-		;
+		/*
+				rgba_code = $("body").css('background-color');
+				//rgba(100,100,100,0.9)
+				console.log('rgba');
+				console.log(rgba_code);
+				code = rgba_code.match(/(\d+,)+\d*(.\d)/);
+				//'100,100,100,0.9'
+				
+				console.log('code部分');
+				console.log(code);
+				new_code = code[0].replace(/\d+(\.\d)*$/, '0.2');
+				//'100,100,100,0.8'
+				//new_code = new_code.match(/\d+(.*)$/)[1].replace(', ', '')
+				//'0.8'
+				console.log('新カラーコード');
+				console.log(new_code);
+				//codeの
+				'100, 100, 100, 0.8'.replace(/new_code/, '0.5');
+				$("body").css('background-color').replace(/new_code/, '0.5');
+				if (rgba_code.match(/\,/).index == 3) {
+					//alphaある前提
+		
+				} else {
+					//rgbのみの場合
+		
+				}
+				;
+				*/
 		addAlpha($("body"), emotion_level[$(this).val()]);
 	});
 });
